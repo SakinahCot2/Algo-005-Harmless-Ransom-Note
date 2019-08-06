9b4766b246d19d1f88a4cedb19d4f47da5d5e9f0
Sakinah Cottman <sakinah.cottman@yahoo.com>
2019-08-06 09:59:11 -0400

    Attempt

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,18 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+        String note [] = magazineText.split(" ");
+        Map<String, Integer> ransomMap = new HashMap<String, Integer>();
+        for(String ransom:note){
+            if(ransomMap.get(ransom) >1){
+                return false;
+            }
+        }
+        return true;
     }
 }
